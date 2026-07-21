2ee489ace5e5e795a0d4004221abe403ddfd7fde
Oleksandr Shevchenko <oleksandrs@sapling-inc.com>
2026-07-21 19:07:58 +0300

    Add led blink app.
    Signed-off-by: Oleksandr Shevchenko <oleksandrs@sapling-inc.com>

---
+++ F4Matrix/src_led_blink/main.c
@@ -0,0 +1,195 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F4xx_StdPeriph_Templates/main.c
+  * @author  MCD Application Team
+  * @version V1.4.0
+  * @date    04-August-2014
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+
+
+TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+TIM_OCInitTypeDef  TIM_OCInitStructure;
+__IO uint16_t CCR1_Val = 0xFFFF;
+__IO uint16_t CCR2_Val = 27309;
+__IO uint16_t CCR3_Val = 13654;
+__IO uint16_t CCR4_Val = 6826;
+uint16_t PrescalerValue = 0;
+
+void TIM_Config(void);
+
+void TIM_Config(void)
+{
+  static GPIO_InitTypeDef GPIO_InitStructure;
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  /* TIM3 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+  /* Enable the TIM3 global Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+//  60   GPIO_InitStructure.GPIO_Pin = LED1_PIN | LED2_PIN;
+//  61   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+//  62   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+//  63   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+//  64   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+//  65   GPIO_Init(GPIOG, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_High_Speed;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14|GPIO_Pin_7;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	/* Set PG6 and PG8 */
+	GPIOB->BSRR = (uint32_t)(GPIO_Pin_14 | GPIO_Pin_7);
+
+  // /* Initialize Leds mounted on STM324xG-EVAL board */
+  // STM_EVAL_LEDInit(LED1);
+  // STM_EVAL_LEDInit(LED2);
+  // STM_EVAL_LEDInit(LED3);
+  // STM_EVAL_LEDInit(LED4);
+
+  // /* Turn on LED1, LED2, LED3 and LED4 */
+  // STM_EVAL_LEDOn(LED1);
+  // STM_EVAL_LEDOn(LED2);
+  // STM_EVAL_LEDOn(LED3);
+  // STM_EVAL_LEDOn(LED4);
+}
+
+
+
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       files (startup_stm32f40_41xxx.s/startup_stm32f427_437xx.s/startup_stm32f429_439xx.s)
+       before to branch to application main. 
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f4xx.c file
+     */
+
+  /* TIM Configuration */
+  TIM_Config();
+  
+  /* -----------------------------------------------------------------------
+    TIM3 Configuration: Output Compare Timing Mode:
+
+    SystemCoreClock = 168 MHz
+
+    In this example TIM3 input clock (TIM3CLK) is set to 2 * APB1 clock (PCLK1), 
+    since APB1 prescaler is different from 1.   
+      TIM3CLK = 2 * PCLK1  
+      PCLK1 = HCLK / 4 
+      => TIM3CLK = HCLK / 2 = SystemCoreClock /2
+          
+    To get TIM3 counter clock at 6 MHz, the prescaler is computed as follows:
+       Prescaler = (TIM3CLK / TIM3 counter clock) - 1
+       Prescaler = ((SystemCoreClock /2) /6 MHz) - 1
+                                              
+    CC1 update rate = TIM3 counter clock / CCR1_Val = 91.55 Hz
+
+    C2 update rate = TIM3 counter clock / CCR2_Val = 219.7 Hz
+
+    CC3 update rate = TIM3 counter clock / CCR3_Val = 439.4 Hz
+
+    CC4 update rate = TIM3 counter clock / CCR4_Val = 878.9 Hz
+
+
+    Note: 
+     SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f4xx.c file.
+     Each time the core clock (HCLK) changes, user had to call SystemCoreClockUpdate()
+     function to update SystemCoreClock variable value. Otherwise, any configuration
+     based on this variable will be incorrect.    
+  ----------------------------------------------------------------------- */   
+
+
+  /* Compute the prescaler value */
+  PrescalerValue = (uint16_t) ((SystemCoreClock / 2) / 6000000) - 1;
+
+  /* Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = 65535;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+  /* Prescaler configuration */
+  TIM_PrescalerConfig(TIM3, PrescalerValue, TIM_PSCReloadMode_Immediate);
+
+  /* Output Compare Timing Mode configuration: Channel1 */
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+  TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel2 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
+
+  TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel3 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR3_Val;
+
+  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel4 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
+
+  TIM_OC4Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Disable);
+   
+  /* TIM Interrupts enable */
+  TIM_ITConfig(TIM3, TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3 | TIM_IT_CC4, ENABLE);
+
+  /* TIM3 enable counter */
+  TIM_Cmd(TIM3, ENABLE);
+
+  while (1);
+}
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
